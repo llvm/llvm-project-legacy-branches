654cd3ffbd9da6ea19fbd52c1d3235ddd81b40c8
Robert Bocchino <bocchino@illinois.edu>
2005-10-18 19:49:02 +0000

    Initial commit of Vector LLVM.
    llvm-svn: 23794

--- llvm/lib/Transforms/Vector/LowerFixedVector.cpp
+++ llvm/lib/Transforms/Vector/LowerFixedVector.cpp
@@ -1,14 +1,14 @@
-//===- LowerPacked.cpp -  Implementation of LowerPacked Transform ---------===//
-//
+//===- LowerFixedVector.cpp -  Implementation of LowerFixedVector Transform ---------===//
+// 
 //                     The LLVM Compiler Infrastructure
 //
 // This file was developed by Brad Jones and is distributed under
 // the University of Illinois Open Source License. See LICENSE.TXT for details.
-//
+// 
 //===----------------------------------------------------------------------===//
 //
-// This file implements lowering Packed datatypes into more primitive
-// Packed datatypes, and finally to scalar operations.
+// This file implements lowering FixedVector datatypes into more primitive
+// FixedVector datatypes, and finally to scalar operations.
 //
 //===----------------------------------------------------------------------===//
 
@@ -29,97 +29,96 @@ using namespace llvm;
 
 namespace {
 
-/// This pass converts packed operators to an
-/// equivalent operations on smaller packed data, to possibly
-/// scalar operations.  Currently it supports lowering
-/// to scalar operations.
+/// This pass converts fixed-length vector operators to an equivalent
+/// operations on smaller fixed-length vector data, to possibly scalar
+/// operations.  Currently it supports lowering to scalar operations.
 ///
-/// @brief Transforms packed instructions to simpler instructions.
+/// @brief Transforms fixed-length vector instructions to simpler instructions.
 ///
-class LowerPacked : public FunctionPass, public InstVisitor<LowerPacked> {
+class LowerFixedVector : public FunctionPass, public InstVisitor<LowerFixedVector> {
 public:
-   /// @brief Lowers packed operations to scalar operations.
+   /// @brief Lowers fixed-length vector operations to scalar operations. 
    /// @param F The fuction to process
    virtual bool runOnFunction(Function &F);
 
-   /// @brief Lowers packed load instructions.
+   /// @brief Lowers fixed-length vector load instructions.
    /// @param LI the load instruction to convert
    void visitLoadInst(LoadInst& LI);
 
-   /// @brief Lowers packed store instructions.
+   /// @brief Lowers fixed-length vector store instructions.
    /// @param SI the store instruction to convert
    void visitStoreInst(StoreInst& SI);
 
-   /// @brief Lowers packed binary operations.
+   /// @brief Lowers fixed-length vector binary operations.
    /// @param BO the binary operator to convert
    void visitBinaryOperator(BinaryOperator& BO);
 
-   /// @brief Lowers packed select instructions.
+   /// @brief Lowers fixed-length vector select instructions.
    /// @param SELI the select operator to convert
    void visitSelectInst(SelectInst& SELI);
 
-   /// This function asserts if the instruction is a PackedType but
+   /// This function asserts if the instruction is a FixedVectorType but
    /// is handled by another function.
-   ///
-   /// @brief Asserts if PackedType instruction is not handled elsewhere.
+   /// 
+   /// @brief Asserts if FixedVectorType instruction is not handled elsewhere.
    /// @param I the unhandled instruction
    void visitInstruction(Instruction &I)
    {
-      if(isa<PackedType>(I.getType())) {
-         std::cerr << "Unhandled Instruction with Packed ReturnType: " <<
+      if(isa<FixedVectorType>(I.getType())) {
+         std::cerr << "Unhandled Instruction with FixedVector ReturnType: " << 
                       I << '\n';
       }
    }
 private:
-   /// @brief Retrieves lowered values for a packed value.
-   /// @param val the packed value
+   /// @brief Retrieves lowered values for a fixed-length vector value.
+   /// @param val the fixed-length vector value
    /// @return the lowered values
    std::vector<Value*>& getValues(Value* val);
 
-   /// @brief Sets lowered values for a packed value.
-   /// @param val the packed value
+   /// @brief Sets lowered values for a fixed-length vector value.
+   /// @param val the fixed-length vector value
    /// @param values the corresponding lowered values
    void setValues(Value* val,const std::vector<Value*>& values);
 
    // Data Members
-   /// @brief whether we changed the function or not
+   /// @brief whether we changed the function or not   
    bool Changed;
 
-   /// @brief a map from old packed values to new smaller packed values
-   std::map<Value*,std::vector<Value*> > packedToScalarMap;
+   /// @brief a map from old fixed-length vector values to new smaller fixed-length vector values
+   std::map<Value*,std::vector<Value*> > fixedVectorToScalarMap;
 
    /// Instructions in the source program to get rid of
-   /// after we do a pass (the old packed instructions)
+   /// after we do a pass (the old fixed-length vector instructions)
    std::vector<Instruction*> instrsToRemove;
-};
+}; 
 
-RegisterOpt<LowerPacked>
-X("lower-packed",
-  "lowers packed operations to operations on smaller packed datatypes");
+RegisterOpt<LowerFixedVector> 
+X("lower-fixed-vector", 
+  "lowers fixed-length vector operations to operations on smaller fixed-length vector datatypes");
 
-} // end namespace
+} // end namespace   
 
-FunctionPass *llvm::createLowerPackedPass() { return new LowerPacked(); }
+FunctionPass *llvm::createLowerFixedVectorPass() { return new LowerFixedVector(); }
 
 
 // This function sets lowered values for a corresponding
-// packed value.  Note, in the case of a forward reference
-// getValues(Value*) will have already been called for
-// the packed parameter.  This function will then replace
-// all references in the in the function of the "dummy"
-// value the previous getValues(Value*) call
+// fixed-length vector value.  Note, in the case of a forward reference
+// getValues(Value*) will have already been called for 
+// the fixed-length vector parameter.  This function will then replace 
+// all references in the in the function of the "dummy" 
+// value the previous getValues(Value*) call 
 // returned with actual references.
-void LowerPacked::setValues(Value* value,const std::vector<Value*>& values)
+void LowerFixedVector::setValues(Value* value,const std::vector<Value*>& values)
 {
-   std::map<Value*,std::vector<Value*> >::iterator it =
-         packedToScalarMap.lower_bound(value);
-   if (it == packedToScalarMap.end() || it->first != value) {
+   std::map<Value*,std::vector<Value*> >::iterator it = 
+         fixedVectorToScalarMap.lower_bound(value);
+   if (it == fixedVectorToScalarMap.end() || it->first != value) {
        // there was not a forward reference to this element
-       packedToScalarMap.insert(it,std::make_pair(value,values));
+       fixedVectorToScalarMap.insert(it,std::make_pair(value,values));
    }
    else {
       // replace forward declarations with actual definitions
-      assert(it->second.size() == values.size() &&
+      assert(it->second.size() == values.size() && 
              "Error forward refences and actual definition differ in size");
       for (unsigned i = 0, e = values.size(); i != e; ++i) {
            // replace and get rid of old forward references
@@ -130,74 +129,74 @@ void LowerPacked::setValues(Value* value,const std::vector<Value*>& values)
    }
 }
 
-// This function will examine the packed value parameter
-// and if it is a packed constant or a forward reference
+// This function will examine the fixed-length vector value parameter
+// and if it is a fixed-length vector constant or a forward reference
 // properly create the lowered values needed.  Otherwise
-// it will simply retreive values from a
-// setValues(Value*,const std::vector<Value*>&)
+// it will simply retreive values from a  
+// setValues(Value*,const std::vector<Value*>&) 
 // call.  Failing both of these cases, it will abort
 // the program.
-std::vector<Value*>& LowerPacked::getValues(Value* value)
+std::vector<Value*>& LowerFixedVector::getValues(Value* value)
 {
-   assert(isa<PackedType>(value->getType()) &&
-          "Value must be PackedType");
+   assert(isa<FixedVectorType>(value->getType()) &&
+          "Value must be FixedVectorType");
 
    // reject further processing if this one has
    // already been handled
-   std::map<Value*,std::vector<Value*> >::iterator it =
-      packedToScalarMap.lower_bound(value);
-   if (it != packedToScalarMap.end() && it->first == value) {
+   std::map<Value*,std::vector<Value*> >::iterator it = 
+      fixedVectorToScalarMap.lower_bound(value);
+   if (it != fixedVectorToScalarMap.end() && it->first == value) {
        return it->second;
    }
 
-   if (ConstantPacked* CP = dyn_cast<ConstantPacked>(value)) {
+   if (ConstantVector* CP = dyn_cast<ConstantVector>(value)) {
        // non-zero constant case
        std::vector<Value*> results;
        results.reserve(CP->getNumOperands());
        for (unsigned i = 0, e = CP->getNumOperands(); i != e; ++i) {
           results.push_back(CP->getOperand(i));
        }
-       return packedToScalarMap.insert(it,
+       return fixedVectorToScalarMap.insert(it,
                                        std::make_pair(value,results))->second;
    }
    else if (ConstantAggregateZero* CAZ =
             dyn_cast<ConstantAggregateZero>(value)) {
-       // zero constant
-       const PackedType* PKT = cast<PackedType>(CAZ->getType());
+       // zero constant 
+       const FixedVectorType* PKT = cast<FixedVectorType>(CAZ->getType());
        std::vector<Value*> results;
        results.reserve(PKT->getNumElements());
-
+   
        Constant* C = Constant::getNullValue(PKT->getElementType());
        for (unsigned i = 0, e = PKT->getNumElements(); i != e; ++i) {
             results.push_back(C);
        }
-       return packedToScalarMap.insert(it,
+       return fixedVectorToScalarMap.insert(it,
                                        std::make_pair(value,results))->second;
    }
    else if (isa<Instruction>(value)) {
        // foward reference
-       const PackedType* PKT = cast<PackedType>(value->getType());
+       const FixedVectorType* PKT = cast<FixedVectorType>(value->getType());
        std::vector<Value*> results;
        results.reserve(PKT->getNumElements());
-
+   
       for (unsigned i = 0, e = PKT->getNumElements(); i != e; ++i) {
            results.push_back(new Argument(PKT->getElementType()));
       }
-      return packedToScalarMap.insert(it,
+      return fixedVectorToScalarMap.insert(it,
                                       std::make_pair(value,results))->second;
    }
    else {
        // we don't know what it is, and we are trying to retrieve
        // a value for it
-       assert(false && "Unhandled PackedType value");
+       assert(false && "Unhandled FixedVectorType value");
        abort();
    }
 }
 
-void LowerPacked::visitLoadInst(LoadInst& LI)
+void LowerFixedVector::visitLoadInst(LoadInst& LI)
 {
-   // Make sure what we are dealing with is a packed type
-   if (const PackedType* PKT = dyn_cast<PackedType>(LI.getType())) {
+   // Make sure what we are dealing with is a fixed-length vector type
+   if (const FixedVectorType* PKT = dyn_cast<FixedVectorType>(LI.getType())) {
        // Initialization, Idx is needed for getelementptr needed later
        std::vector<Value*> Idx(2);
        Idx[0] = ConstantUInt::get(Type::UIntTy,0);
@@ -206,7 +205,7 @@ void LowerPacked::visitLoadInst(LoadInst& LI)
                                       PKT->getNumElements());
        PointerType* APT = PointerType::get(AT);
 
-       // Cast the packed type to an array
+       // Cast the fixed-length vector type to an array
        Value* array = new CastInst(LI.getPointerOperand(),
                                    APT,
                                    LI.getName() + ".a",
@@ -221,43 +220,43 @@ void LowerPacked::visitLoadInst(LoadInst& LI)
             Idx[1] = ConstantUInt::get(Type::UIntTy,i);
 
             // Get the pointer
-            Value* val = new GetElementPtrInst(array,
+            Value* val = new GetElementPtrInst(array, 
                                                Idx,
-                                               LI.getName() +
+                                               LI.getName() + 
                                                ".ge." + utostr(i),
                                                &LI);
 
-            // generate the new load and save the result in packedToScalar map
-            values.push_back(new LoadInst(val,
+            // generate the new load and save the result in fixedVectorToScalar map
+            values.push_back(new LoadInst(val, 
                              LI.getName()+"."+utostr(i),
                              LI.isVolatile(),
                              &LI));
        }
-
+               
        setValues(&LI,values);
        Changed = true;
        instrsToRemove.push_back(&LI);
    }
 }
 
-void LowerPacked::visitBinaryOperator(BinaryOperator& BO)
+void LowerFixedVector::visitBinaryOperator(BinaryOperator& BO)
 {
-   // Make sure both operands are PackedTypes
-   if (isa<PackedType>(BO.getOperand(0)->getType())) {
+   // Make sure both operands are FixedVectorTypes
+   if (isa<FixedVectorType>(BO.getOperand(0)->getType())) {
        std::vector<Value*>& op0Vals = getValues(BO.getOperand(0));
        std::vector<Value*>& op1Vals = getValues(BO.getOperand(1));
        std::vector<Value*> result;
        assert((op0Vals.size() == op1Vals.size()) &&
-              "The two packed operand to scalar maps must be equal in size.");
+              "The two fixed-length vector operand to scalar maps must be equal in size.");
 
        result.reserve(op0Vals.size());
-
+   
        // generate the new binary op and save the result
        for (unsigned i = 0; i != op0Vals.size(); ++i) {
-            result.push_back(BinaryOperator::create(BO.getOpcode(),
-                                                    op0Vals[i],
+            result.push_back(BinaryOperator::create(BO.getOpcode(), 
+                                                    op0Vals[i], 
                                                     op1Vals[i],
-                                                    BO.getName() +
+                                                    BO.getName() + 
                                                     "." + utostr(i),
                                                     &BO));
        }
@@ -268,94 +267,94 @@ void LowerPacked::visitBinaryOperator(BinaryOperator& BO)
    }
 }
 
-void LowerPacked::visitStoreInst(StoreInst& SI)
+void LowerFixedVector::visitStoreInst(StoreInst& SI)
 {
-   if (const PackedType* PKT =
-       dyn_cast<PackedType>(SI.getOperand(0)->getType())) {
+   if (const FixedVectorType* PKT = 
+       dyn_cast<FixedVectorType>(SI.getOperand(0)->getType())) {
        // We will need this for getelementptr
        std::vector<Value*> Idx(2);
        Idx[0] = ConstantUInt::get(Type::UIntTy,0);
-
+         
        ArrayType* AT = ArrayType::get(PKT->getContainedType(0),
                                       PKT->getNumElements());
        PointerType* APT = PointerType::get(AT);
 
-       // cast the packed to an array type
+       // cast the fixed-length vector to an array type
        Value* array = new CastInst(SI.getPointerOperand(),
                                    APT,
                                    "store.ge.a.",
                                    &SI);
        std::vector<Value*>& values = getValues(SI.getOperand(0));
-
+      
        assert((values.size() == PKT->getNumElements()) &&
-              "Scalar must have the same number of elements as Packed Type");
+              "Scalar must have the same number of elements as FixedVector Type");
 
        for (unsigned i = 0, e = PKT->getNumElements(); i != e; ++i) {
             // Generate the indices for getelementptr
             Idx[1] = ConstantUInt::get(Type::UIntTy,i);
-            Value* val = new GetElementPtrInst(array,
+            Value* val = new GetElementPtrInst(array, 
                                                Idx,
                                                "store.ge." +
                                                utostr(i) + ".",
                                                &SI);
             new StoreInst(values[i], val, SI.isVolatile(),&SI);
        }
-
+                 
        Changed = true;
        instrsToRemove.push_back(&SI);
    }
 }
 
-void LowerPacked::visitSelectInst(SelectInst& SELI)
+void LowerFixedVector::visitSelectInst(SelectInst& SELI)
 {
-   // Make sure both operands are PackedTypes
-   if (isa<PackedType>(SELI.getType())) {
+   // Make sure both operands are FixedVectorTypes
+   if (isa<FixedVectorType>(SELI.getType())) {
        std::vector<Value*>& op0Vals = getValues(SELI.getTrueValue());
        std::vector<Value*>& op1Vals = getValues(SELI.getFalseValue());
        std::vector<Value*> result;
 
       assert((op0Vals.size() == op1Vals.size()) &&
-             "The two packed operand to scalar maps must be equal in size.");
+             "The two fixed-length vector operand to scalar maps must be equal in size.");
 
       for (unsigned i = 0; i != op0Vals.size(); ++i) {
            result.push_back(new SelectInst(SELI.getCondition(),
-                                           op0Vals[i],
+                                           op0Vals[i], 
                                            op1Vals[i],
                                            SELI.getName()+ "." + utostr(i),
                                            &SELI));
       }
-
+   
       setValues(&SELI,result);
       Changed = true;
       instrsToRemove.push_back(&SELI);
    }
 }
 
-bool LowerPacked::runOnFunction(Function& F)
+bool LowerFixedVector::runOnFunction(Function& F)
 {
    // initialize
-   Changed = false;
-
+   Changed = false; 
+  
    // Does three passes:
-   // Pass 1) Converts Packed Operations to
-   //         new Packed Operations on smaller
+   // Pass 1) Converts FixedVector Operations to 
+   //         new FixedVector Operations on smaller
    //         datatypes
    visit(F);
-
+  
    // Pass 2) Drop all references
    std::for_each(instrsToRemove.begin(),
                  instrsToRemove.end(),
                  std::mem_fun(&Instruction::dropAllReferences));
 
-   // Pass 3) Delete the Instructions to remove aka packed instructions
-   for (std::vector<Instruction*>::iterator i = instrsToRemove.begin(),
-                                            e = instrsToRemove.end();
+   // Pass 3) Delete the Instructions to remove aka fixed-length vector instructions
+   for (std::vector<Instruction*>::iterator i = instrsToRemove.begin(), 
+                                            e = instrsToRemove.end(); 
         i != e; ++i) {
-        (*i)->getParent()->getInstList().erase(*i);
+        (*i)->getParent()->getInstList().erase(*i);   
    }
 
    // clean-up
-   packedToScalarMap.clear();
+   fixedVectorToScalarMap.clear();
    instrsToRemove.clear();
 
    return Changed;
