ea21bd14cada7d119ae1390e96d7b78e73330382
Bill Wendling <isanbard@gmail.com>
2008-12-08 22:27:44 +0000

    Creating llvmCore-2088 branch from r60617.
    llvm-svn: 60723

--- clang/test/CodeGen/unsupported.c
+++
@@ -1,6 +0,0 @@
-// RUN: clang -verify -emit-llvm -o %t %s 
-
-int f0(int x) {
-  int vla[x]; // expected-error {{cannot codegen this variable-length array yet}}
-  return vla[x-1];
-}
--- clang/test/Sema/i-c-e3.c
+++
@@ -1,3 +0,0 @@
-// RUN: clang %s -fsyntax-only -verify
-
-int a() {int p; *(1 ? &p : (void*)(0 && (a(),1))) = 10;}
