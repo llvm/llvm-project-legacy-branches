f96377c0cdafaf1b5249d4d71dfeaf2b9143283e
Steve Naroff <snaroff@apple.com>
2009-02-04 17:55:23 +0000

    Branch the rewriter.
    llvm-svn: 63744

--- clang/test/TestRunner.sh
+++ clang/test/TestRunner.sh
@@ -1,103 +1,36 @@
 #!/bin/sh
 #
-#  TestRunner.sh - This script is used to run arbitrary unit tests.  Unit
-#  tests must contain the command used to run them in the input file, starting
-#  immediately after a "RUN:" string.
+#  TestRunner.sh - This script is used to run the deja-gnu tests exactly like
+#  deja-gnu does, by executing the Tcl script specified in the test case's 
+#  RUN: lines. This is made possible by a simple make target supported by the
+#  test/Makefile. All this script does is invoke that make target. 
 #
-#  This runner recognizes and replaces the following strings in the command:
+#  Usage:
+#     TestRunner.sh {script_names}
 #
-#     %s - Replaced with the input name of the program, or the program to
-#          execute, as appropriate.
-#     %prcontext - prcontext.tcl script
-#     %t - temporary file name (derived from testcase name)
+#     This script is typically used by cd'ing to a test directory and then
+#     running TestRunner.sh with a list of test file names you want to run.
 #
-
-FILENAME=$1
-TESTNAME=$1
-SUBST=$1
-
-OUTPUT=Output/$1.out
-
-# create the output directory if it does not already exist
-mkdir -p `dirname $OUTPUT` > /dev/null 2>&1
-
-if test $# != 1; then
-  # If more than one parameter is passed in, there must be three parameters:
-  # The filename to read from (already processed), the command used to execute,
-  # and the file to output to.
-  SUBST=$2
-  OUTPUT=$3
-  TESTNAME=$3
+TESTPATH=`pwd`
+SUBDIR=""
+if test `dirname $1` = "." ; then
+  while test `basename $TESTPATH` != "test" -a ! -z "$TESTPATH" ; do
+    tmp=`basename $TESTPATH`
+    SUBDIR="$tmp/$SUBDIR"
+    TESTPATH=`dirname $TESTPATH`
+  done
 fi
 
-ulimit -t 40
-
-# Verify the script contains a run line.
-grep -q 'RUN:' $FILENAME || ( 
-   echo "******************** TEST '$TESTNAME' HAS NO RUN LINE! ********************"
-   exit 1
-)
-
-# Run under valgrind if the VG environment variable has been set.
-CLANG=$CLANG
-if [ ! -n "$CLANG" ]; then
-    CLANG="clang"
-fi
-if [ -n "$VG" ]; then
-  rm -f $OUTPUT.vg
-  CLANG="valgrind --leak-check=full --quiet --log-file=$OUTPUT.vg $CLANG"
-fi
-
-SCRIPT=$OUTPUT.script
-TEMPOUTPUT=$OUTPUT.tmp
-grep 'RUN:' $FILENAME | \
-  sed -e "s|^.*RUN:\(.*\)$|\1|g" \
-      -e "s|clang|$CLANG|g" \
-      -e "s|%s|$SUBST|g" \
-      -e "s|%prcontext|prcontext.tcl|g" \
-      -e "s|%t|$TEMPOUTPUT|g" > $SCRIPT
-
-IS_XFAIL=0
-if (grep -q XFAIL $FILENAME); then
-    IS_XFAIL=1
-    printf "XFAILED '$TESTNAME': "
-    grep XFAIL $FILENAME
-fi
-
-/bin/sh $SCRIPT > $OUTPUT 2>&1
-SCRIPT_STATUS=$?
-
-if [ -n "$VG" ]; then
-  [ ! -s $OUTPUT.vg ]
-  VG_STATUS=$?
-else
-  VG_STATUS=0
-fi
-
-if [ $IS_XFAIL -ne 0 ]; then
-    if [ $SCRIPT_STATUS -ne 0 ]; then
-        SCRIPT_STATUS=0
+for TESTFILE in "$@" ; do 
+  if test `dirname $TESTFILE` = . ; then
+    if test -d "$TESTPATH" ; then
+      cd $TESTPATH
+      make check-one TESTONE="$SUBDIR$TESTFILE"
+      cd $PWD
     else
-        SCRIPT_STATUS=1
+      echo "Can't find llvm/test directory in " `pwd`
     fi
-fi
-
-if [ $SCRIPT_STATUS -ne 0 -o $VG_STATUS -ne 0 ]; then
-  echo "******************** TEST '$TESTNAME' FAILED! ********************"
-  echo "Command: "
-  cat $SCRIPT
-  if [ $SCRIPT_STATUS -eq 0 ]; then
-    echo "Output:"
-  elif [ $IS_XFAIL -ne 0 ]; then
-    echo "Incorrect Output (Expected Failure):"
   else
-    echo "Incorrect Output:"
+    make check-one TESTONE=$TESTFILE
   fi
-  cat $OUTPUT
-  if [ $VG_STATUS -ne 0 ]; then
-    echo "Valgrind Output:"
-    cat $OUTPUT.vg
-  fi
-  echo "******************** TEST '$TESTNAME' FAILED! ********************"
-  exit 1
-fi
+done
