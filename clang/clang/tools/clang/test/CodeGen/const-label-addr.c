9919b85b0632264d17231b737bbdc4e2f054669f
Mike Stump <mrs@apple.com>
2009-02-11 01:41:00 +0000

    Update.
    llvm-svn: 64279

--- clang/clang/tools/clang/test/CodeGen/const-label-addr.c
+++
@@ -1,4 +0,0 @@
-// RUN: clang %s -emit-llvm -o %t
-int a() {
-A:;static void* a = &&A;
-}
--- clang/clang/tools/clang/test/CodeGen/emit-all-decls.c
+++
@@ -1,8 +0,0 @@
-// RUN: clang -emit-llvm -o %t %s &&
-// RUN: not grep "@foo" %t &&
-// RUN: clang -femit-all-decls -emit-llvm -o %t %s &&
-// RUN: grep "@foo" %t
-
-static void foo() {
-  
-}
