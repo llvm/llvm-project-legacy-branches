ajohnson-uoregon/clang-rewrite-only

commit 0f404e98fead349f63ad64cf02c78361cc60006f

When emitting instructions that define EFLAGS and the EFLAGS value isn't used, mark the defs as dead.
llvm-svn: 61215

llvm/lib/Target/X86/X86RegisterInfo.cpp
@@ -403,6 +403,9 @@ eliminateCallFramePseudoInstr(MachineFunction &MF, MachineBasicBlock &MBB,
         }
       }
 
+      // The EFLAGS implicit def is dead.
+      New->getOperand(3).setIsDead();
+
       // Replace the pseudo instruction with a new instruction...
       if (New) MBB.insert(I, New);
     }
@@ -416,6 +419,9 @@ eliminateCallFramePseudoInstr(MachineFunction &MF, MachineBasicBlock &MBB,
         (Is64Bit ? X86::SUB64ri32 : X86::SUB32ri);
       MachineInstr *New =
         BuildMI(MF, TII.get(Opc), StackPtr).addReg(StackPtr).addImm(CalleeAmt);
+      // The EFLAGS implicit def is dead.
+      New->getOperand(3).setIsDead();
+
       MBB.insert(I, New);
     }
   }
@@ -517,7 +523,10 @@ void emitSPUpdate(MachineBasicBlock &MBB, MachineBasicBlock::iterator &MBBI,
 
   while (Offset) {
     uint64_t ThisVal = (Offset > Chunk) ? Chunk : Offset;
-    BuildMI(MBB, MBBI, TII.get(Opc), StackPtr).addReg(StackPtr).addImm(ThisVal);
+    MachineInstr *MI =
+      BuildMI(MBB, MBBI, TII.get(Opc), StackPtr).addReg(StackPtr).addImm(ThisVal);
+    // The EFLAGS implicit def is dead.
+    MI->getOperand(3).setIsDead();
     Offset -= ThisVal;
   }
 }
@@ -713,8 +722,11 @@ void X86RegisterInfo::emitPrologue(MachineFunction &MF) const {
   // applies to tail call optimized functions where the callee argument stack
   // size is bigger than the callers.
   if (TailCallReturnAddrDelta < 0) {
-    BuildMI(MBB, MBBI, TII.get(Is64Bit? X86::SUB64ri32 : X86::SUB32ri),
-            StackPtr).addReg(StackPtr).addImm(-TailCallReturnAddrDelta);
+    MachineInstr *MI =
+      BuildMI(MBB, MBBI, TII.get(Is64Bit? X86::SUB64ri32 : X86::SUB32ri),
+              StackPtr).addReg(StackPtr).addImm(-TailCallReturnAddrDelta);
+    // The EFLAGS implicit def is dead.
+    MI->getOperand(3).setIsDead();
   }
 
   uint64_t NumBytes = 0;
@@ -751,10 +763,14 @@ void X86RegisterInfo::emitPrologue(MachineFunction &MF) const {
       I->addLiveIn(FramePtr);
 
     // Realign stack
-    if (needsStackRealignment(MF))
-      BuildMI(MBB, MBBI,
-              TII.get(Is64Bit ? X86::AND64ri32 : X86::AND32ri),
-              StackPtr).addReg(StackPtr).addImm(-MaxAlign);
+    if (needsStackRealignment(MF)) {
+      MachineInstr *MI =
+        BuildMI(MBB, MBBI,
+                TII.get(Is64Bit ? X86::AND64ri32 : X86::AND32ri),
+                StackPtr).addReg(StackPtr).addImm(-MaxAlign);
+      // The EFLAGS implicit def is dead.
+      MI->getOperand(3).setIsDead();
+    }
   } else
     NumBytes = StackSize - X86FI->getCalleeSavedFrameSize();
 

llvm/llvm-project-legacy-branches

commit 591f00627c75591788379e56c7cf03c4e18955fa

Merge r64524 from release_25:
Don't try to set an EFLAGS operand to dead if no instruction was created.
This fixes a bug introduced by r61215.

llvm-svn: 64593

llvm/lib/Target/X86/X86RegisterInfo.cpp
@@ -413,11 +413,13 @@ eliminateCallFramePseudoInstr(MachineFunction &MF, MachineBasicBlock &MBB,
         }
       }
 
-      // The EFLAGS implicit def is dead.
-      New->getOperand(3).setIsDead();
+      if (New) {
+        // The EFLAGS implicit def is dead.
+        New->getOperand(3).setIsDead();
 
-      // Replace the pseudo instruction with a new instruction...
-      if (New) MBB.insert(I, New);
+        // Replace the pseudo instruction with a new instruction...
+        MBB.insert(I, New);
+      }
     }
   } else if (I->getOpcode() == getCallFrameDestroyOpcode()) {
     // If we are performing frame pointer elimination and if the callee pops