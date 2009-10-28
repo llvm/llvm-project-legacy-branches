cd6dfa2659a0d849a184154a01989f6076f138e6
Ted Kremenek <kremenek@apple.com>
2009-10-28 18:34:13 +0000

    Merge in r85132.
    llvm-svn: 85413

--- clang/test/Coverage/targets.c
+++ clang/test/Coverage/targets.c
@@ -15,4 +15,8 @@
 // RUN: clang-cc -g -triple sparc-unknown-unknown -emit-llvm -o %t %s &&
 // RUN: clang-cc -g -triple sparc-unknown-solaris -emit-llvm -o %t %s &&
 // RUN: clang-cc -g -triple pic16-unknown-unknown -emit-llvm -o %t %s &&
+
+// <rdar://problem/7181838> clang 1.0 fails to compile Python 2.6
+// RUN: clang-cc -g -triple x86_64-apple-darwin9 -emit-llvm -o %t %s -mmacosx-version-min=10.4 &&
+
 // RUN: true
