822632ab86f15d7d4d82d7c861d79f911f5339e7
Jon Ziegler <jonz@apple.com>
2009-11-03 22:43:50 +0000

    Update.
    llvm-svn: 85959

--- clang/clang/tools/clang/test/CXX/dcl.dcl/basic.namespace/namespace.udecl/p6-cxx0x.cpp
+++
@@ -1,8 +0,0 @@
-// RUN: clang-cc -fsyntax-only -verify %s
-// C++0x N2914.
-
-namespace A {
-  namespace B { }
-}
-
-using A::B; // expected-error{{using declaration can not refer to namespace}}
--- clang/clang/tools/clang/test/CodeGenCXX/nullptr.cpp
+++
@@ -1,7 +0,0 @@
-// RUN: clang-cc -std=c++0x  %s -emit-llvm -o %t
-
-int* a = nullptr;
-
-void f() {
-  int* a = nullptr;
-}
