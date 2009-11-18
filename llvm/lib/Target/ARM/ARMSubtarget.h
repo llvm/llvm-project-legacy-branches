11ef81fe5f29c8b8e6a2fbf26f797af39a42b401
Bill Wendling <isanbard@gmail.com>
2009-11-18 19:46:27 +0000

    $ svn merge -c 87015 https://llvm.org/svn/llvm-project/llvm/trunk --- Merging r87015 into '.': U    lib/CodeGen/ScheduleDAG.cpp U    lib/CodeGen/LatencyPriorityQueue.cpp U    lib/CodeGen/AggressiveAntiDepBreaker.cpp U    lib/CodeGen/PostRASchedulerList.cpp $ svn merge -c 88682 https://llvm.org/svn/llvm-project/llvm/trunk --- Merging r88682 into '.': U    include/llvm/Target/TargetSubtarget.h G    lib/CodeGen/AggressiveAntiDepBreaker.cpp G    lib/CodeGen/PostRASchedulerList.cpp U    lib/CodeGen/AggressiveAntiDepBreaker.h U    lib/Target/ARM/ARMSubtarget.cpp U    lib/Target/ARM/ARMSubtarget.h U    lib/Target/X86/X86Subtarget.h U    lib/Target/X86/X86Subtarget.cpp U    lib/Target/TargetSubtarget.cpp $ svn merge -c 89218 https://llvm.org/svn/llvm-project/llvm/trunk --- Merging r89218 into '.': U    lib/Target/ARM/ARMScheduleV7.td U    lib/Target/ARM/ARMScheduleV6.td U    lib/Target/ARM/ARM.td
    llvm-svn: 89228

--- llvm/lib/Target/ARM/ARMSubtarget.h
+++ llvm/lib/Target/ARM/ARMSubtarget.h
@@ -135,7 +135,7 @@ protected:
   /// enablePostRAScheduler - True at 'More' optimization.
   bool enablePostRAScheduler(CodeGenOpt::Level OptLevel,
                              TargetSubtarget::AntiDepBreakMode& Mode,
-                             ExcludedRCVector& ExcludedRCs) const;
+                             RegClassVector& CriticalPathRCs) const;
 
   /// getInstrItins - Return the instruction itineraies based on subtarget
   /// selection.
