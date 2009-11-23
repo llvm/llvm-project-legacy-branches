b4c1a3a7a7aeb776155ea6dce96ca1298c28af93
Jim Grosbach <grosbach@apple.com>
2009-11-23 20:44:02 +0000

    revert inadvertant bits of previous commit
    llvm-svn: 89698

--- llvm/lib/Analysis/IVUsers.cpp
+++ llvm/lib/Analysis/IVUsers.cpp
@@ -33,7 +33,7 @@ static RegisterPass<IVUsers>
 X("iv-users", "Induction Variable Users", false, true);
 
 static cl::opt<bool>
-SimplifyIVUsers("simplify-iv-users", cl::Hidden, cl::init(true),
+SimplifyIVUsers("simplify-iv-users", cl::Hidden, cl::init(false),
           cl::desc("Restrict IV Users to loop-invariant strides"));
 
 Pass *llvm::createIVUsersPass() {
