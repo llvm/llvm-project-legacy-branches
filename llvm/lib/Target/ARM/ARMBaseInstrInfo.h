41ab948e76fd5f4e16a8197f9a5b1c20825750f0
Bob Wilson <bob.wilson@apple.com>
2009-11-30 18:45:36 +0000

    --- Merging r90144 into '.': U    include/llvm/Target/TargetInstrInfo.h U    lib/CodeGen/TailDuplication.cpp U    lib/Target/PowerPC/PPCInstrInfo.h U    lib/Target/ARM/ARMSubtarget.cpp U    lib/Target/ARM/ARMSubtarget.h U    lib/Target/ARM/ARMBaseInstrInfo.cpp U    lib/Target/ARM/ARMBaseInstrInfo.h U    lib/Target/X86/X86InstrInfo.h
    llvm-svn: 90146

--- llvm/lib/Target/ARM/ARMBaseInstrInfo.h
+++ llvm/lib/Target/ARM/ARMBaseInstrInfo.h
@@ -274,8 +274,6 @@ public:
 
   virtual bool isIdentical(const MachineInstr *MI, const MachineInstr *Other,
                            const MachineRegisterInfo *MRI) const;
-
-  virtual bool isProfitableToDuplicateIndirectBranch() const;
 };
 
 static inline
