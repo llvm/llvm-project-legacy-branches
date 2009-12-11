fc5fa6dd44d302b354521046d211ae1f90c967f3
Bill Wendling <isanbard@gmail.com>
2009-12-11 21:54:06 +0000

    Revert stack protector change r91164 after tagging.
    llvm-svn: 91167

--- llvm/lib/CodeGen/StackProtector.cpp
+++ llvm/lib/CodeGen/StackProtector.cpp
@@ -112,6 +112,10 @@ bool StackProtector::RequiresStackProtector() const {
           return true;
 
         if (const ArrayType *AT = dyn_cast<ArrayType>(AI->getAllocatedType())) {
+          // We apparently only care about character arrays.
+          if (AT->getElementType() != Type::getInt8Ty(AT->getContext()))
+            continue;
+
           // If an array has more than SSPBufferSize bytes of allocated space,
           // then we emit stack protectors.
           if (SSPBufferSize <= TD->getTypeAllocSize(AT))
