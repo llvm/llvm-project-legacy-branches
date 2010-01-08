367e2fc5d55e833632c0508724517cacdd3afb8d
Jon Ziegler <jonz@apple.com>
2010-01-08 18:24:58 +0000

    Retag.
    llvm-svn: 93013

--- clang/clang/tools/clang/test/CodeGenCXX/reference-init.cpp
+++
@@ -1,9 +0,0 @@
-// RUN: %clang_cc1 -emit-llvm-only -verify %s
-
-struct XPTParamDescriptor {};
-struct nsXPTParamInfo {
-  nsXPTParamInfo(const XPTParamDescriptor& desc);
-};
-void a(XPTParamDescriptor *params) {
-  const nsXPTParamInfo& paramInfo = params[0];
-}
