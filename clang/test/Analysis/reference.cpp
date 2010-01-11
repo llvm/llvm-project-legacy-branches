3ea7c9f5e62bc51c967fb7ccd5d1564ab4ae2f1d
Ted Kremenek <kremenek@apple.com>
2010-01-11 02:38:32 +0000

    Removing checker-233.
    llvm-svn: 93139

--- clang/test/Analysis/reference.cpp
+++
@@ -1,6 +0,0 @@
-// RUN: %clang_cc1 -analyze -analyzer-experimental-internal-checks -checker-cfref -analyzer-store=region -analyzer-constraints=range -verify %s
-
-void f1() {
-  int const &i = 3;
-  int b = i;
-}
