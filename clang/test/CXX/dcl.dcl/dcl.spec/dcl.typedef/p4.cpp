7dbc8aea7ce36daf7e996053681af88ec493b3c9
Ted Kremenek <kremenek@apple.com>
2010-01-19 00:38:12 +0000

    Removing checker-233.
    llvm-svn: 93826

--- clang/test/CXX/dcl.dcl/dcl.spec/dcl.typedef/p4.cpp
+++
@@ -1,9 +0,0 @@
-// RUN: %clang_cc1 -verify %s
-// XFAIL: *
-
-struct S {
-  typedef struct A {} A; // expected-note {{previous definition is here}}
-  typedef struct B B;
-  typedef A A; // expected-error {{redefinition of 'A'}}
-};
-
