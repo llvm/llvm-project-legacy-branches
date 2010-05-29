9d85d5074fede6d6620b0e56bbd03b10a41e5c90
Daniel Dunbar <daniel@zuster.org>
2010-05-29 00:27:25 +0000

    Update to draw from Whitney branch.
    llvm-svn: 105069

--- clang/ClangBNI.mk
+++ clang/ClangBNI.mk
@@ -223,7 +223,7 @@ update-sources:
 	fi
 	svn rm -m 'Update.' $(SVN_CLANG)/src
 	svn cp -m 'Update.' $(SVN_BASE)/llvm/branches/Apple/whitney@$(REVISION) $(SVN_CLANG)/src
-	svn cp -m 'Update.' $(SVN_BASE)/cfe/trunk@$(REVISION) $(SVN_CLANG)/src/tools/clang
+	svn cp -m 'Update.' $(SVN_BASE)/cfe/branches/Apple/whitney@$(REVISION) $(SVN_CLANG)/src/tools/clang
 	svn cp -m 'Update.' $(SVN_BASE)/compiler-rt/trunk@$(REVISION) $(SVN_CLANG)/src/projects/compiler-rt
 	svn up
 
