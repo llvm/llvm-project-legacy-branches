51cf3c04eee2140e86656fdebb10dd7c97fbe849
Daniel Dunbar <daniel@zuster.org>
2010-07-19 23:34:14 +0000

    Update.
    llvm-svn: 108776

--- clang/src/tools/clang/test/Analysis/PR7218.c
+++
@@ -1,6 +0,0 @@
-// RUN: %clang_cc1 -analyze -analyzer-check-objc-mem -analyzer-store region -verify %s
-char PR7218(char a) {
-    char buf[2];
-    buf[0] = a;
-    return buf[1]; // expected-warning {{Undefined or garbage value returned to caller}}
-}
