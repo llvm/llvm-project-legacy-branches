317c81bd2b5ada643336aa5cc4ac31e20efc28fd
Daniel Dunbar <daniel@zuster.org>
2010-10-01 03:42:13 +0000

    Merge r115260: -- Author: Douglas Gregor <dgregor@apple.com> Date:   Fri Oct 1 01:05:22 2010 +0000
        Enable chained precompiled headers for use with precompiled preambles
    
    llvm-svn: 115281

--- clang/lib/Frontend/ASTUnit.cpp
+++ clang/lib/Frontend/ASTUnit.cpp
@@ -1169,9 +1169,7 @@ llvm::MemoryBuffer *ASTUnit::getMainBufferWithPrecompiledPreamble(
   
   // Tell the compiler invocation to generate a temporary precompiled header.
   FrontendOpts.ProgramAction = frontend::GeneratePCH;
-  // FIXME: Set ChainedPCH unconditionally, once it is ready.
-  if (::getenv("LIBCLANG_CHAINING"))
-    FrontendOpts.ChainedPCH = true;
+  FrontendOpts.ChainedPCH = true;
   // FIXME: Generate the precompiled header into memory?
   FrontendOpts.OutputFile = PreamblePCHPath;
   
