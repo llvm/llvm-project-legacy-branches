6c57b2a4790aec47ea780c1f9bb131f9ba681b55
Eric Christopher <echristo@apple.com>
2011-01-08 01:49:27 +0000

    Use tar to copy sources over, excluding a number of directories. Add a TODO for less copying.
    llvm-svn: 123050

--- clang/ClangBNI.mk
+++ clang/ClangBNI.mk
@@ -438,7 +438,7 @@ FIND		:= /usr/bin/find
 INSTALL		:= /usr/bin/install
 INSTALL_FILE	:= $(INSTALL) -m 0444
 MKDIR		:= /bin/mkdir -p -m 0755
-PAX		:= /bin/pax
+TAR		:= /usr/bin/tar
 RMDIR		:= /bin/rm -fr
 XARGS		:= /usr/bin/xargs
 
@@ -481,12 +481,13 @@ install: install-clang
 
 clean:
 
+# We can --exclude src/tools/clang/test/ when the top level makefile stops
+# trying to create a test directory when IS_TOP_LEVEL.
 installsrc:
 	@echo "Installing source..."
 	$(_v) $(MKDIR) "$(SRCROOT)"
-	$(_v) $(PAX) -rw . "$(SRCROOT)"
+	$(_v) $(TAR) -cL --exclude src/test/ --exclude src/projects/test-suite --exclude src/projects/compiler-rt -f - .  | (cd "$(SRCROOT)"; $(TAR) xpf - )
 	$(_v) $(FIND) "$(SRCROOT)" $(Find_Cruft) -depth -exec $(RMDIR) "{}" \;
-	$(_v) rm -rf "$(SRCROOT)"/src/test/*/
 	$(_v) rm -rf "$(SRCROOT)"/src/tools/clang/test/*/
 
 installhdrs:
