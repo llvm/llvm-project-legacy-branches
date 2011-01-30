0631b51b87fe50db950a72246e2371dd45187cac
Jakob Stoklund Olesen <stoklund@2pi.dk>
2011-01-30 22:12:11 +0000

    $ svn merge -c 124564 https://llvm.org/svn/llvm-project/llvm/trunk --- Merging r124564 into '.': U    lib/CodeGen/TailDuplication.cpp
    Permit -tail-dup-size to work even when optimizing for size.
    
    llvm-svn: 124571

--- llvm/lib/CodeGen/TailDuplication.cpp
+++ llvm/lib/CodeGen/TailDuplication.cpp
@@ -457,7 +457,8 @@ TailDuplicatePass::TailDuplicate(MachineBasicBlock *TailBB, MachineFunction &MF,
   // duplicate only one, because one branch instruction can be eliminated to
   // compensate for the duplication.
   unsigned MaxDuplicateCount;
-  if (MF.getFunction()->hasFnAttr(Attribute::OptimizeForSize))
+  if (TailDuplicateSize.getNumOccurrences() == 0 &&
+      MF.getFunction()->hasFnAttr(Attribute::OptimizeForSize))
     MaxDuplicateCount = 1;
   else
     MaxDuplicateCount = TailDuplicateSize;
