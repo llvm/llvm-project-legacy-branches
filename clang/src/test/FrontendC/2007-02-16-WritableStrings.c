0756701bd99ab548c462cfb3cc125f3cd9f3d911
David Dean <david_dean@apple.com>
2011-02-04 18:14:25 +0000

    Update.
    llvm-svn: 124881

--- clang/src/test/FrontendC/2007-02-16-WritableStrings.c
+++
@@ -1,7 +0,0 @@
-// Test the -fwritable-strings option.
-
-// RUN: %llvmgcc -O3 -S -o - -fwritable-strings %s | \
-// RUN:    grep {internal global}
-// RUN: %llvmgcc -O3 -S -o - %s | grep {private constant}
-
-char *X = "foo";
