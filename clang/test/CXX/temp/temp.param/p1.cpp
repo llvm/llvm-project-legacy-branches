daae51e0639087d9206ab4718c10a3e2c19e34c4
Daniel Dunbar <daniel@zuster.org>
2012-02-27 21:01:43 +0000

    Decay.
    llvm-svn: 151556

--- clang/test/CXX/temp/temp.param/p1.cpp
+++
@@ -1,6 +0,0 @@
-// Suppress 'no run line' failure.
-// RUN: %clang_cc1 -fsyntax-only -verify %s
-
-template<template<> class C> class D; // expected-error{{template template parameter must have its own template parameters}}
-
-
