9301d9cb17758dd6bb73beb882d5077fccfa9d34
Chandler Carruth <chandlerc@gmail.com>
2013-08-27 17:04:52 +0000

    Cleaning up testing branch
    llvm-svn: 189373

--- llvm/test/MC/ARM/deprecated-v8.s
+++
@@ -1,3 +0,0 @@
-@ RUN: llvm-mc -triple armv8 -show-encoding < %s 2>&1 | FileCheck %s
-setend be
-@ CHECK: warning: deprecated on armv8
--- llvm/utils/lit/lit/formats/__init__.py
+++
@@ -1,4 +0,0 @@
-from __future__ import absolute_import
-from lit.formats.base import FileBasedTest, OneCommandPerFileTest
-from lit.formats.googletest import GoogleTest
-from lit.formats.shtest import ShTest
