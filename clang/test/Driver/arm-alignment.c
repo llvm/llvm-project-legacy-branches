5b5f7b99412cec426f7e8ee99dc460cd01479c5d
Nick Lewycky <nicholas@mxc.ca>
2013-10-15 00:24:46 +0000

    Cleaning up testing branch
    llvm-svn: 192647

--- clang/test/Driver/arm-alignment.c
+++
@@ -1,9 +0,0 @@
-// RUN: %clang -target arm-none-gnueeabi -munaligned-access -### %s 2> %t
-// RUN: FileCheck --check-prefix=CHECK-UNALIGNED < %t %s
-
-// CHECK-UNALIGNED: "-backend-option" "-arm-no-strict-align"
-
-// RUN: %clang -target arm-none-gnueeabi -mno-unaligned-access -### %s 2> %t
-// RUN: FileCheck --check-prefix=CHECK-ALIGNED < %t %s
-
-// CHECK-ALIGNED: "-backend-option" "-arm-strict-align"
--- clang/test/Driver/xcore-opts.c
+++
@@ -1,7 +0,0 @@
-// RUN: %clang -target xcore %s -### -o %t.o 2>&1 FileCheck %s
-
-// CHECK: "-momit-leaf-frame-pointer"
-// CHECK-NOT: "-mdisable-fp-elim"
-// CHECK: "-fno-signed-char"
-// CHECK: "-fno-common"
-
