ac767ea7e93b553e295bb13842d42962fc9cebb8
Gabor Greif <ggreif@gmail.com>
2014-03-05 11:02:58 +0000

    assert an essential size invariant
    llvm-svn: 202965

--- llvm/lib/IR/Use.cpp
+++ llvm/lib/IR/Use.cpp
@@ -164,6 +164,8 @@ Use *Use::initTags<false>(Use * const Start, Use *Stop) {
 
 template<>
 Use *Use::initTags<true>(Use * const Start, Use *Stop) {
+# define static_assert(COND) typedef char dummy[(COND) ? 1 : -1]
+  static_assert(sizeof(Use) == 3*sizeof(Use**));
   typedef PrevPointerIntPair<true> T;
   ptrdiff_t Done = 0;
   while (Done < 17) {
