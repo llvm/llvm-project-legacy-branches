185676692ce52534ecd54136fedd7e1bb6d9e1aa
Nick Lewycky <nicholas@mxc.ca>
2014-04-19 21:04:10 +0000

    Cleaning up testing branch
    llvm-svn: 206689

--- clang-tools-extra/test/clang-tidy/basic.cpp
+++
@@ -1,7 +0,0 @@
-// RUN: grep -Ev "// *[A-Z-]+:" %s > %t.cpp
-// RUN: clang-tidy %t.cpp -checks='llvm-namespace-comment' -disable-checks='' -- > %t2.cpp
-// RUN: FileCheck -input-file=%t2.cpp %s
-
-namespace i {
-}
-// CHECK: warning: namespace not terminated with a closing comment [llvm-namespace-comment]
