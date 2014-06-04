4dca55a9c94af00dfa2b59528d8c0b7d8559d92a
Nick Lewycky <nicholas@mxc.ca>
2014-06-04 04:36:12 +0000

    Cleaning up testing branch
    llvm-svn: 210149

--- clang/test/CodeGen/builtins-arm64.c
+++
@@ -1,6 +0,0 @@
-// RUN: %clang_cc1 -triple arm64-apple-ios -O3 -emit-llvm -o - %s | FileCheck %s
-
-void f0(void *a, void *b) {
-	__clear_cache(a,b);
-// CHECK: call {{.*}} @__clear_cache
-}
--- llvm/test/MC/COFF/invalid-endef.s
+++
@@ -1,5 +0,0 @@
-# RUN: not llvm-mc -triple i686-windows -filetype obj -o /dev/null %s
-# REQUIRES: asserts
-
-	.endef
-
