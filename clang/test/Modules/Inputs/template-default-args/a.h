58bd8d50dda542c0059f0c2f505a683ae3b026d0
Alexey Samsonov <vonosmas@gmail.com>
2015-08-01 00:03:47 +0000

    Cleaning up testing branch
    llvm-svn: 243803

--- clang/test/Modules/Inputs/template-default-args/a.h
+++
@@ -1,7 +0,0 @@
-template<typename T = int> struct A {};
-template<typename T> struct B {};
-template<typename T> struct C;
-template<typename T> struct D;
-template<typename T> struct E;
-template<typename T = int> struct G;
-template<typename T = int> struct H;
