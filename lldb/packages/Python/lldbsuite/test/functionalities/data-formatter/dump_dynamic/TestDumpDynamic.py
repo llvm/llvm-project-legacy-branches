7c59fc9694980102e5d47d83dc3acbfbad4cf181
Richard Trieu <rtrieu@google.com>
2016-03-18 18:37:46 +0000

    Cleaning up testing branch
    llvm-svn: 263803

--- lldb/packages/Python/lldbsuite/test/functionalities/data-formatter/dump_dynamic/TestDumpDynamic.py
+++
@@ -1,5 +0,0 @@
-from __future__ import absolute_import
-
-from lldbsuite.test import lldbinline
-
-lldbinline.MakeInlineTest(__file__, globals(), [lldbinline.expectedFailureWindows("llvm.org/pr24663")])
--- lldb/packages/Python/lldbsuite/test/functionalities/data-formatter/stringprinter/TestStringPrinter.py
+++
@@ -1,4 +0,0 @@
-import lldbsuite.test.lldbinline as lldbinline
-import lldbsuite.test.lldbtest as lldbtest
-
-lldbinline.MakeInlineTest(__file__, globals(), [lldbtest.expectedFailureWindows("llvm.org/pr24772")])
