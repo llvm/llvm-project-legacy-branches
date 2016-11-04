362c3a1a4016867a8d3ca89a4d536226125d3d92
Nick Lewycky <nicholas@mxc.ca>
2016-11-04 21:10:23 +0000

    Cleaning up testing branch
    llvm-svn: 286011

--- clang-tools-extra/test/clang-tidy/list-checks.cpp
+++
@@ -1,5 +0,0 @@
-// REQUIRES: shell
-// RUN: mkdir -p %T/clang-tidy/list-checks/
-// RUN: echo '{Checks: "-*,google-*"}' > %T/clang-tidy/.clang-tidy
-// RUN: cd %T/clang-tidy/list-checks
-// RUN: clang-tidy -list-checks | grep "^ *google-"
