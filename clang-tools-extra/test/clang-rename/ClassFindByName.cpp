2409bd223693d4d5d19bc4ae5aa45e2f05b64e38
Eric Christopher <echristo@gmail.com>
2016-11-22 17:29:45 +0000

    Cleaning up stable branch
    llvm-svn: 287648

--- clang-tools-extra/test/clang-rename/ClassFindByName.cpp
+++
@@ -1,9 +0,0 @@
-// RUN: clang-rename -old-name=Foo -new-name=Bar %s -- | FileCheck %s
-
-class Foo {         // CHECK: class Bar
-};
-
-int main() {
-  Foo *Pointer = 0; // CHECK: Bar *Pointer = 0;
-  return 0;
-}
