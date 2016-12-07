a8cc3eae92bfca6098624c7afa75d8ca0e70b87f
Eric Christopher <echristo@gmail.com>
2016-12-07 18:06:26 +0000

    Cleaning up testing branch
    llvm-svn: 288940

--- clang/test/Driver/XRay/xray-instrument-cpu.c
+++
@@ -1,4 +0,0 @@
-// RUN: not %clang -o /dev/null -v -fxray-instrument -c %s
-// XFAIL: amd64-, x86_64-, x86_64h-, arm
-// REQUIRES: linux
-typedef int a;
--- clang/test/Driver/XRay/xray-instrument-os.c
+++
@@ -1,4 +0,0 @@
-// RUN: not %clang -o /dev/null -v -fxray-instrument -c %s
-// XFAIL: -linux-
-// REQUIRES-ANY: amd64, x86_64, x86_64h, arm
-typedef int a;
