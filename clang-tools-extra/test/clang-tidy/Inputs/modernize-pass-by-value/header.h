e46be317b7d5ab042bba770a7226d43cb2e94a40
Eric Christopher <echristo@gmail.com>
2017-03-17 21:56:02 +0000

    Cleaning up testing branch
    llvm-svn: 298141

--- clang-tools-extra/test/clang-tidy/Inputs/modernize-pass-by-value/header.h
+++
@@ -1,7 +0,0 @@
-class ThreadId {
-};
-
-struct A {
-  A(const ThreadId &tid) : threadid(tid) {}
-  ThreadId threadid;
-};
--- clang/test/Coverage/ast-printing.cpp
+++
@@ -1,9 +0,0 @@
-// RUN: %clang_cc1 -fsyntax-only %s
-// RUN: %clang_cc1 -ast-print %s -o %t.1.cpp
-// RUN: %clang_cc1 -ast-print %t.1.cpp -o %t.2.cpp
-// RUN: diff %t.1.cpp %t.2.cpp
-// RUN: %clang_cc1 -ast-dump %s
-// RUN: %clang_cc1 -print-decl-contexts %s
-// RUN: %clang_cc1 -fdump-record-layouts %s
-
-#include "cxx-language-features.inc"
