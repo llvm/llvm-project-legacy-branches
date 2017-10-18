dd32f13ce3a0bf90fb556d467b86aea678fb470a
Daniel Jasper <djasper@google.com>
2017-10-18 19:18:51 +0000

    Cleaning up stable branch
    llvm-svn: 316110

--- clang/test/Refactor/LocalRename/Field.cpp
+++
@@ -1,9 +0,0 @@
-// RUN: clang-refactor local-rename -selection=test:%s %s -- | FileCheck %s
-
-class Baz {
-  int /*range=*/Foo; // CHECK: int /*range=*/Bar;
-public:
-  Baz();
-};
-
-Baz::Baz() : /*range=*/Foo(0) {}  // CHECK: Baz::Baz() : /*range=*/Bar(0) {};
